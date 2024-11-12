b4bf1fcfe863f9a544006db784223353ba3c159c
JANGUK <janguk95@naver.com>
2024-11-12 17:34:45 +0900

    JWT start

--- Backend/src/main/java/com/fitwork/back/member/model/service/MemberService.java
+++ Backend/src/main/java/com/fitwork/back/member/model/service/MemberService.java
@@ -1,5 +1,15 @@
 package com.fitwork.back.member.model.service;
 
-public interface MemberService {
+import com.fitwork.back.member.model.dto.Member;
 
+public interface MemberService {
+	
+	public boolean joinMember(Member member);
+	
+	public Member getMemberInfo(String id);
+	
+	public boolean deleteMember(String id);
+	
+	public boolean modifyMemberInfo(Member member);
+	
 }
